790c3dcf107a717271020bf5e7e63dbe92002b7f
Juan Jerrah <juanjerrah15@gmail.com>
2022-01-06 09:32:37 -0300

    Base Criada

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express, {Request, Response} from 'express';
+import path from 'path';
+import mustache from 'mustache-express';
+import dotenv from 'dotenv';
+import mainRoutes from './routes/index'
+const server = express();
+
+//Ativa Dotenv
+dotenv.config();
+
+//Ativa o mustache
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+//Mostra o caminho da pasta public
+server.use(express.static(path.join(__dirname, '../public')));
+
+//Possibilita pegar dados da url por meio de requisição GET
+server.use(express.urlencoded({extended:true}));
+
+//Ativa as rotas
+server.use(mainRoutes);
+
+
+//Pagina 404
+server.get('/', (req: Request, res: Response)=>{
+    res.status(404).send('ERROR 404 - Page Not Found!')
+});
+
+
+
+//Porta de execução do servidor
+server.listen(process.env.PORT);
+
+
+
